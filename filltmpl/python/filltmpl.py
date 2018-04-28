d7a2ec660bc71806fe6987606a39c3e16ff736da
Lincoln de Sousa <lincoln@comum.org>
2018-04-28 15:02:03 -0400

    Add filltmpl compiler in python

--- filltmpl/python/filltmpl.py
+++ filltmpl/python/filltmpl.py
@@ -5,10 +5,20 @@ eg_env = {
     'y': ['base cases', 'recursion'],
 }
 
+### Purely interpreted version
+
 def fill(t, env):
-    if   t    == '': return ''
+    if   t    == '':  return ''
     elif t[0] == '$': return env[t[1]]               + fill(t[2:], env)
     elif t[0] == '@': return ' and '.join(env[t[1]]) + fill(t[2:], env)
-    else: return t[0]                                + fill(t[1:], env)
-
+    else:             return t[0]                    + fill(t[1:], env)
 print(fill(eg_template, eg_env))
+
+### Compiled into functions
+
+def make_filler(t):
+    if   t    == '':  return lambda env: ''
+    elif t[0] == '$': return lambda env: env[t[1]]                + make_filler(t[2:])(env)
+    elif t[0] == '@': return lambda env: ' and '.join(env[t[1]])  + make_filler(t[2:])(env)
+    else:             return lambda env: t[0]                     + make_filler(t[1:])(env)
+print(make_filler(eg_template)(eg_env))
