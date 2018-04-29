830e8f73aa3f59a93925b06f3c517cd1fe77acab
Lincoln de Sousa <lincoln@comum.org>
2018-04-28 20:28:07 -0400

    Add another version of filltmpl in python

--- filltmpl/python/filltmpl.py
+++ filltmpl/python/filltmpl.py
@@ -22,3 +22,15 @@ def make_filler(t):
     elif t[0] == '@': return lambda env: ' and '.join(env[t[1]])  + make_filler(t[2:])(env)
     else:             return lambda env: t[0]                     + make_filler(t[1:])(env)
 print(make_filler(eg_template)(eg_env))
+
+### Compiled into functions & output generation separated
+Empty    = lambda env: ''
+Hole     = lambda name, rest: lambda env: env[name]               + rest(env)
+ListHole = lambda name, rest: lambda env: ' and '.join(env[name]) + rest(env)
+Constant = lambda name, rest: lambda env: name                    + rest(env)
+def make_filler2(t):
+    if   t == '':     return Empty
+    elif t[0] == '$': return Hole    (t[1], make_filler2(t[2:]))
+    elif t[0] == '@': return ListHole(t[1], make_filler2(t[2:]))
+    else:             return Constant(t[0], make_filler2(t[1:]))
+print(make_filler2(eg_template)(eg_env))
